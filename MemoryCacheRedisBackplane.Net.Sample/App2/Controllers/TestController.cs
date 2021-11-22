33c0484d21fe0a0b00586808d0fb119adfe3b0a0
Antoine BIDAULT <antoine.bidault@gmail.com>
2021-11-22 01:32:23 +0100

    Add second project in sample

--- MemoryCacheRedisBackplane.Net.Sample/App2/Controllers/TestController.cs
+++ MemoryCacheRedisBackplane.Net.Sample/App2/Controllers/TestController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace MemoryCacheRedisBackplane.Net.Sample.Controllers;
 
@@ -12,21 +13,48 @@ public class TestController : ControllerBase
     };
 
     private readonly ILogger<TestController> _logger;
+    private readonly IMemoryCache _cache;
+    private IMemoryCacheInvalidator _cacheInvalidator;
+    private string _cacheKey;
 
-    public TestController(ILogger<TestController> logger)
+    public TestController(ILogger<TestController> logger, IMemoryCache cache, IMemoryCacheInvalidator cacheInvalidator)
     {
         _logger = logger;
+        _cache = cache;
+        _cacheInvalidator = cacheInvalidator;
+        _cacheKey = "test";
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+
+    [HttpGet("invalidate")]
+    public IActionResult Invalidate()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        _cacheInvalidator.InvalidateAsync(_cacheKey);
+        return Ok();
+    }
+
+    [HttpGet("fromcache")]
+    public IActionResult Get()
+    {
+        return Ok(GetFromCache());
+
+    }
+
+    private object GetFromCache()
+    {
+        return _cache.GetOrCreate(this._cacheKey, c =>
         {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+            c.AbsoluteExpiration = DateTime.UtcNow.AddDays(1);
+            return new
+            {
+                Data = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                }).ToArray(),
+                CacheTime = DateTime.Now
+            };
+        });
     }
 }
--- MemoryCacheRedisBackplane.Net.Sample/App2/Program.cs
+++ MemoryCacheRedisBackplane.Net.Sample/App2/Program.cs
@@ -8,7 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddMemoryCacheRedisBackplane();
+builder.Services.AddMemoryCache();
+builder.Services.AddMemoryCacheRedisBackplane(builder.Configuration.GetConnectionString("Redis"));
 
 var app = builder.Build();
 
